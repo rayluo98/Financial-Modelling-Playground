3086566978f953ed7cbacfcad178801858d6660f
Raymond Luo <rrl290@nyu.edu>
2024-05-19 20:27:55 -0400

    [ALPHA-001] initial work to split backtest engine between single stock strat and multistrat simulations. review: Benjamin

--- Basket Trade/backtest.py
+++ Basket Trade/backtest.py
@@ -2,6 +2,10 @@ import pandas as pd
 import numpy as np
 from dataclasses import dataclass
 from matplotlib import pyplot as plt
+import order_book
+import logging
+import tqdm
+from concurrent.futures import ThreadPoolExecutor
 
 ### Intuition Behind Backtest Class ###
 # we want to create a module backtest class on an generic strategy
@@ -22,18 +26,44 @@ class backtestEngine(object):
     def __init__(self, btHist: pd.DataFrame):
         self.bt_hist = btHist
 
-    @classmethod
-    def reloadEngine(cls, btHist: pd.DataFrame):
-        cls.bt_hist = btHist
+    def reloadEngine(self, btHist: pd.DataFrame):
+        self.bt_hist = btHist
 
-    @classmethod
-    def applySignal(cls, signal: pd.DataFrame, isMeanRevert: bool=False):
+    def simulateTrade():
+        return None
+
+    def pairsTradeSignal(self, ticker1: str, ticker2: str, tradeQty: float|pd.DataFrame,
+                         signal:pd.DataFrame)->pd.DataFrame:
+        ## initiate orderbook 
+        BBO = order_book.Book()
+        split_idx = np.array_split(df.columns, 16)
+        tasks = [df[i] for i in split_idx]
+        # run do_describe fucntions in the process pool
+        with ThreadPoolExecutor() as executor:
+            result = executor.map(do_describe, tasks)
+        return None
+
+    def singleTradeSignal(bdf: pd.DataFrame, ticker: str, signal:pd.DataFrame, 
+                          tradeQty: float| pd.DataFrame=1)->pd.DataFrame:
+        # now combine all the triggers to see when we are holding or shorting.
+        # with somewhat-continuity of the signal, we can just assume that after a buy, the end-buy will occur BEFORE starting a new sale, and the same for sell->endsell->newbuy.
+        # note, for signing purposes, the end-buy is actually selling.
+        if isinstance(tradeQty, float):
+            bdf['holding_action'] = tradeQty*(1*bdf['trigger_buy'] - 1*bdf['trigger_end_buy'] - 1*bdf['trigger_sell'] + 1*bdf['trigger_end_sell'])
+        else: ## if its dataframe type, check logic here
+            bdf['holding_action'] = tradeQty*(1*bdf['trigger_buy'] - 1*bdf['trigger_end_buy'] - 1*bdf['trigger_sell'] + 1*bdf['trigger_end_sell'])
+        # by design of the boolean triggers and the continuity assumption, the cumulative sum of the actions should range within [1, -1]. check it in the plot.
+        bdf['position'] = bdf['holding_action'].cumsum()
+        return None
+
+    def applySignal(self, tickers: list[str], signal: pd.DataFrame,
+                          tradeQty: float | pd.DataFrame  = 1, isMeanRevert: bool=False):
         ## removes existing signal
-        if ("signal" in cls.bt_hist):
-            cls.bt_hist.drop(cls.bt_cols, errors='ignore', inplace=True, axis=1)
-        cls.bt_hist = cls.bt_hist.merge(signal, on ="Date", how='left')
+        if ("signal" in self.bt_hist):
+            self.bt_hist.drop(self.bt_cols, errors='ignore', inplace=True, axis=1)
+        self.bt_hist = self.bt_hist.merge(signal, on ="Date", how='left')
         # compute boolean masks for when to trigger open position buy/sell
-        bdf = cls.bt_hist
+        bdf = self.bt_hist
         bdf['start_buy'] = bdf['signal'] >= bdf['buy_level']
         bdf['start_sell'] = bdf['signal'] <= bdf['sell_level']
         # combine into a single boolean mask.
@@ -65,23 +95,21 @@ class backtestEngine(object):
         # other than the initialization, the diff should be +2 or -2.
         bdf['trigger_end_buy'] = bdf['cends'].diff() >= 1
         bdf['trigger_end_sell'] = bdf['cends'].diff() <= -1
+        if (len(tickers)==1):
+            bdf = backtestEngine.singleTradeSignal(bdf, tickers, signal, tradeQty)
+        elif (len(tickers)==2):
+            bdf = backtestEngine.pairsTradeSignal(tickers[0], tickers[1], bdf)
+        else:
+            logging.Logger.error("Don't have a handler for strategy more than 2 assets")
 
-        # now combine all the triggers to see when we are holding or shorting.
-        # with somewhat-continuity of the signal, we can just assume that after a buy, the end-buy will occur BEFORE starting a new sale, and the same for sell->endsell->newbuy.
-        # note, for signing purposes, the end-buy is actually selling.
-        bdf['holding_action'] = 1*bdf['trigger_buy'] - 1*bdf['trigger_end_buy'] - 1*bdf['trigger_sell'] + 1*bdf['trigger_end_sell']
-        # by design of the boolean triggers and the continuity assumption, the cumulative sum of the actions should range within [1, -1]. check it in the plot.
-        bdf['position'] = bdf['holding_action'].cumsum()
         return bdf
 
     # calculates the strength of the signal
-    @classmethod 
-    def calcSigStr(cls):
+    def calcSigStr(self):
         return None
         
-    @classmethod
-    def printResult(cls, isMeanRevert: bool=False):
-        bdf = cls.bt_hist
+    def printResult(self, isMeanRevert: bool=False):
+        bdf = self.bt_hist
         plt.figure(figsize=(12,8), dpi=200)
         plt.plot(bdf.index, bdf['signal'])
         if isMeanRevert:
@@ -99,9 +127,8 @@ class backtestEngine(object):
         plt.tight_layout()
         plt.show()
 
-    @classmethod 
-    def getProfit(cls):
-        bdf = cls.bt_hist
+    def getProfit(self):
+        bdf = self.bt_hist
         #compute profit over time, plot the buys and sells on top of the stock price.
         # costs the half spread each time we trade. thus enter+exit position costs 1 entire spread (ask-bid).bdf['tcost'] = (bdf['ask0'] - bdf['bid0']) / 2 * (bdf['trigger_buy'] | bdf['trigger_end_buy'] | bdf['trigger_sell'] | bdf['trigger_end_sell'])
         bdf['mid'] = (bdf['ask0'] + bdf['bid0']) / 2
