7915b3317b47a8f1450ecd6f98adc4eba9730a88
Raymond Luo <rrl290@nyu.edu>
2024-12-27 16:58:10 -0500

    [ALPHA-001] updates to a functional index crawler - no historical. review: self

--- Quant_Trading/JanitorAPI(Crawlers)/IndexCompCrawler.py
+++ Quant_Trading/JanitorAPI(Crawlers)/IndexCompCrawler.py
@@ -3,7 +3,10 @@ import re
 import json
 import os
 import io
+import pandas as pd
 import xmltodict
+import random
+import time
 
 
 DIR = r'C:\Users\raymo\OneDrive\Desktop\Ray Stuff\_Cache'
@@ -18,15 +21,36 @@ class IndexCrawler(object):
         self.url = "https://www.zacks.com/funds/etf/{}/holding"
         self.savDir = DIR
 
-    def crawlerContentCapture(self, response, file_name:str='test.xml'):
-         # saving the xml file 
-        with open(os.path.join(self.savDir, file_name), 'wb') as f: 
-            f.write(response) 
+    def componentWtScrapper(self, txtResponse: str):
+        # Extract Regex Identifiers
+        tableExtract = re.findall(r'<\\/span><\\/span><\\/a>",(.*?)" alt=\\"View Report\\', txtResponse)
+        ### Manual Table Headers
+        headers = ['Shares', 'Pct Weight', 'YoY Change']
+        res = {}
+        errors = {}
+        for row in tableExtract:
+            row_extract = {}
+            ticker_delimiter_idx = row.rfind('/')
+            if (ticker_delimiter_idx == -1):
+                errors[row] = "Couldn't find ticker name in extract!"
+                continue
+            ticker = row[ticker_delimiter_idx + 1:].replace("\\","")
+            table_delimiter_idx = row.find("<")
+            row = row[:table_delimiter_idx].replace(",","")
+            row_split = row.split('\"')
+            for i in range(len(headers)):
+                row_extract[headers[i]] = float(row_split[2*i + 1] if row_split[2*i+1] != "NA" else 0)
+            res[ticker] = row_extract
+        return res
 
-    def componentWtScrapper(self, response, file_name:str='test.xml', elementSearch = r"\<\\ span\>\<\\ span\>\<\\ a\>"):
-        ## custom splitting of html response
-        junkyard = response.split(elementSearch)
-        return junkyard
+    def componentScrapper(self, txtResponse: str):
+        # Extract Regex Identifiers
+        return re.findall(r'etf\\\/(.*?)\\', txtResponse)
+    
+    def janitorsCheckpoint(self, savDir, fileName, df):
+        df.to_csv(os.path.join(savDir, fileName, fileName + "_scraps.csv"))
+        print("oh... so you're done scrapping?")
+
 
     def scrape(self):
         url = self.url
@@ -36,16 +60,17 @@ class IndexCrawler(object):
             req.headers.update(headers)
             for key in self.indices:
                 r = req.get(url.format(key))
-                ### need flag here later
-                self.crawlerContentCapture(r.content, os.path.join(key, key + "_content.xml"))
-                # self.componentWtScrapper(r.text, os.path.join(key, key + "_text.xml"))
                 # json_extract = json.loads(r.text)
                 print(f"Extracting: {r.url}")
-                subquest = re.findall(r'<\\ span><\\ span><\\ a>(.*?) alt=\\"View Snapshot\\', r.text)
-                goal = re.findall(r'etf\\\/(.*?)\\', r.text)
+                subquest = pd.DataFrame(self.componentWtScrapper(r.text)).transpose()
+                # self.janitorsCheckpoint(self.savDir, key, subquest)
+                goal = self.componentScrapper(r.text)
                 print(goal)
                 underlying.append(goal)
                 print(len(goal))
+                ## don't api to get killed for botting 
+                rnd_wait = random.uniform(0,3)
+                time.sleep(rnd_wait)
         return underlying
 
 def main():
